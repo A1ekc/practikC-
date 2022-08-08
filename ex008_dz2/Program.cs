867e6d50aa0043af8c10fd777ef93cdd68647f47
Alex <a1ekcbend@gmail.com>
2022-08-08 15:24:37 +0300

    максимальное из трёх чисел

---
+++ ex008_dz2/Program.cs
@@ -0,0 +1,44 @@
+﻿// Написать программу, которая принимает на вход три числа и выдаёт максимальное из этих чисел.
+
+Console.Write(" Введите два числа(получите максимальное и минимальное): ");
+int a = Convert.ToInt32(Console.ReadLine());//конверт строки в число
+int b = Convert.ToInt32(Console.ReadLine());
+int c = Convert.ToInt32(Console.ReadLine());
+
+
+
+if (a>b)
+{
+    if (a>c)
+    {
+        Console.WriteLine(a +  " максимальное число ");
+    }
+    else 
+    {
+        Console.WriteLine(c +  " максимальное число ");
+    }
+}
+else
+{
+    if (b>c)
+    {
+        Console.WriteLine(b +  " максимальное число ");
+    }
+    else
+     {
+        Console.WriteLine(c +  " максимальное число ");
+    }
+}
+
+/*if (a>b||a>c)
+{
+    Console.WriteLine(a +  " максимальное число ");
+}
+if (b>a||b>c)
+{
+    Console.WriteLine(b +  " максимальное число ");
+}
+if (c>a||c>b)
+{
+    Console.WriteLine(c +  " максимальное число ");
+}*/
