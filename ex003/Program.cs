55109d83197133922ceb78420a9f963e27e95f9d
Alex <a1ekcbend@gmail.com>
2022-08-08 13:56:54 +0300

    проверка двух чисел на квадрат

---
+++ ex003/Program.cs
@@ -0,0 +1,10 @@
+﻿// напишу программу, которая на вход принимает два числа и проверяет, является ли второе число квадратом первогою
+int a = Convert.ToInt32(Console.ReadLine());//конверт строки в число
+int b = Convert.ToInt32(Console.ReadLine());
+
+if (a * a == b)
+{
+    Console.WriteLine(b + " квадрат " + a);
+}
+else   
+    Console.WriteLine(b + " не квадрат " + a);
