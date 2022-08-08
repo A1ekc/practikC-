eaf48c9037c67b404d53a3136927aa5590a78378
Alex <a1ekcbend@gmail.com>
2022-08-08 14:48:34 +0300

    ввод N вывод от -N до N

---
+++ ex005/Program.cs
@@ -0,0 +1,12 @@
+﻿// Напишите программу, которая на вход принимает одно число N , а на выход показывает все целые числа в промежутке от -N до N
+
+Console.Write(" Введите число N ( получите все числа от -N до N): ");
+int number = Convert.ToInt32(Console.ReadLine());
+
+int a = -number;
+while (a < number)
+{
+
+    Console.WriteLine(a+ " ");
+    a++;
+}
