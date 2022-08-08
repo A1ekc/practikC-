7c10f1e97563d5dcd5fe52a6f26c46910eafada2
Alex <a1ekcbend@gmail.com>
2022-08-08 13:17:42 +0300

    квадрат числа

---
+++ ex002/Program.cs
@@ -0,0 +1,5 @@
+﻿// напишу программу, которая на вход принимает число и выдаёт его квадрат.
+Console.Write("Введите число:");
+int number = Convert.ToInt32(Console.ReadLine());//конвертирует строку в число //ввод числа в строку
+int sqrNumber = number * number;
+Console.WriteLine(number + " в квадрате равно " + sqrNumber);
