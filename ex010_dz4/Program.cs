68fe38d488fd0c058fda5a73a89de1fb34d2c023
Alex <a1ekcbend@gmail.com>
2022-08-08 15:39:40 +0300

    чётные числа от 1 до N

---
+++ ex010_dz4/Program.cs
@@ -0,0 +1,14 @@
+﻿// Напишите программу, которая на вход принимает чилсо N, а на выходе показывает все чётные числа от 1 до N
+
+Console.Write(" Введите чиcло N(получите числа от 1 до N ): ");
+int a = Convert.ToInt32(Console.ReadLine());//конверт строки в число
+int count = 0;
+while (count < a)
+{
+    Console.WriteLine(count+ " ");
+    count= count+2;
+}
+
+
+
+
