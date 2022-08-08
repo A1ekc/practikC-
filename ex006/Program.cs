08d619b36408f9fabc8d633d9ed2b171381bbeb4
Alex <a1ekcbend@gmail.com>
2022-08-08 14:49:49 +0300

    последне симвл трёхзначного числа

---
+++ ex006/Program.cs
@@ -0,0 +1,9 @@
+﻿//программа принимает на вход трёхзначное число а на выход выводит три последние цифры
+Console.Write(" Введите трёхзначное число: ");
+int number = Convert.ToInt32(Console.ReadLine());
+if (number > 999 || number < 100)
+{
+    Console.WriteLine("Введено неверное число");
+}
+else
+Console.WriteLine("Результат: "+number%10);
