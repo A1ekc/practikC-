44b0ca3af0bc9918329062567db75bb940c9dbe4
Alex <a1eksbend@gmail.com>
2022-08-09 22:31:08 +0300

    вывод рандомного числа на отрезке и отображения большей цифры из двух

---
+++ ex015_funk/Program.cs
@@ -0,0 +1,30 @@
+﻿// Программа которая выводит случайное число из отрезка [10,99] и показывает наибольшую цифру числа
+
+int randomNumber = GetRandomNumber(10,99);//случайное число в отрезке
+Console.WriteLine(randomNumber);
+
+int firstDigit =randomNumber /10;// первая цифра
+int secondDigit = randomNumber % 10;//вторая цифра %-остаток от деления на 10
+
+
+int maxDigit = Max(firstDigit, secondDigit);//вызываем метод и передаём ему два числа
+Console.WriteLine(maxDigit);
+
+int GetRandomNumber( int min, int max)//создаём метод для выбора случайного числа
+{
+     return new Random().Next(min, max + 1);
+}
+
+int Max(int a, int b)//метод для сравнения двух чисел
+{
+    int max =0;
+    if (a > b)
+    {
+        max = a;
+    }
+    else
+    {
+        max = b;
+    }
+    return max;
+}
