28ba677563e406a83cf894666c6d8d806ba622d9
Alex <a1ekcbend@gmail.com>
2022-08-10 17:03:48 +0300

    квадрат числа

---
+++ ex019_2_4icla_kvadrat/Program.cs
@@ -0,0 +1,25 @@
+﻿// Программа принимает на вход два числа и проверяет является ли одно число квадратом дргугого
+
+int Readint(string messege)//метод
+{
+    Console.WriteLine(messege);
+    return Convert.ToInt32(Console.ReadLine());
+}
+
+bool IsitSqr(int a, int b)//method
+{
+    return (a * a == b);
+}
+
+int FirstNumber = Readint("Первое число: ");
+int SecondNumber = Readint("Второе число: ");
+
+if (IsitSqr(FirstNumber,SecondNumber)|| IsitSqr(SecondNumber,FirstNumber))
+{
+    Console.WriteLine("yes");
+}
+else
+{
+    Console.WriteLine("no");
+}
+
