5bd7d71549333a7d9857e58b4e28eb8c3af9bd1d
Alex <a1eksbend@gmail.com>
2022-08-10 23:32:03 +0300

    второе число из ххх

---
+++ ex020_dz5/Program.cs
@@ -0,0 +1,16 @@
+﻿// Программа, которая принимает на вход трёхзначное число и на выход показывет вторую цифру этого числа.
+int randomNumber = GetRandomNumber(100,999);//случайное число в отрезке
+Console.WriteLine(randomNumber);
+Console.WriteLine(SecondNumberOrRandomNumber(randomNumber));
+
+int GetRandomNumber(int min, int max) //метод для выбора случайного числа
+{
+    return new Random().Next(min, max + 1);
+}
+
+int SecondNumberOrRandomNumber (int randomNumber)//поиск второго числа в трёхзначном числе
+{
+    int xxDigit = randomNumber / 10; //получаем первые 2 числа
+    int SecondNumberOrRandomNumber = xxDigit % 10; //нужное число
+    return SecondNumberOrRandomNumber;
+}
