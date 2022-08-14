9275a167ccc7220a75a004a26910499ec972f120
Alex <A1ekcbend@gmail.com>
2022-08-14 22:57:24 +0300

    полиндром

---
+++ ex027_dz8_polindrom/Program.cs
@@ -0,0 +1,20 @@
+﻿// Программа которая принимает на вход пятизначное число и проверяет является ли оно полиндромом
+//не работает
+
+string polindrom = ReadInt("Введите число N: ");
+
+if ((polindrom[1]==polindrom[5])&&(polindrom[2]==polindrom[4]))
+{
+    Console.WriteLine("Это полиндром ");
+}
+else
+{
+    Console.WriteLine("Это не полиндром ");
+}
+
+string ReadInt(string message)
+{
+    Console.Write(message);
+    return Convert.ToString(Console.ReadLine()); 
+   
+}
