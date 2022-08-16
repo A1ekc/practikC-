9af00752f89b2b1140092d0086ae780c7adbd27c
Alex <A1ekcbend@gmail.com>
2022-08-16 23:03:01 +0300

    сортировка массива

---
+++ ex032_zamena_v_massive/Program.cs
@@ -0,0 +1,33 @@
+﻿// сартировка массива
+
+int[] arr = { 1, 5, 4, 3, 2, 6, 7, 1, 1};
+
+void PrintArray (int[] array)
+{
+    int count = array.Length;//поиск длины массива
+    for (int i = 0; i < count; i++)
+    {
+        Console.Write($"{array[i]}");// вывод массива
+    }
+    Console.WriteLine();
+}
+
+void SelectionSort(int[] array)
+{
+    for (int i = 0; i < array.Length - 1; i++)//-1 из за i +1
+    {
+        int minPosition = i;
+        for (int j = i + 1; j < array.Length; j++)//поиск максимального
+        {
+            if(array[j] < array[minPosition]) minPosition = j;
+        }
+        int temporary = array[i];
+        array[i] = array[minPosition];
+        array[minPosition] = temporary;
+    }
+}
+
+
+PrintArray(arr);
+SelectionSort(arr);
+PrintArray(arr);
