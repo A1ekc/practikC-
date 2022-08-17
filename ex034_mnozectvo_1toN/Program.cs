643f31c13ff0df7fef931820ac95c79d70374b9d
Alex <A1ekcbend@gmail.com>
2022-08-17 22:42:07 +0300

    произведение чисел от 1 до N

---
+++ ex034_mnozectvo_1toN/Program.cs
@@ -0,0 +1,19 @@
+﻿// Принимает на вход число N и выдаёт произведение чисел от 1 до N
+
+int a = ReadInt("Введите число N: ");
+int result = 1;
+
+
+for (int i = 1; i <= a; i++)
+{
+    result *= i;//result = result * i;
+}
+Console.WriteLine($"{result}");
+
+
+int ReadInt(string message)
+{
+    Console.Write(message);
+    return Convert.ToInt32(Console.ReadLine()); 
+}
+
