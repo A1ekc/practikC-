24c80a6d37385e59c368bf827b08ffeffe39e860
Alex <A1ekcbend@gmail.com>
2022-08-18 09:28:38 +0300

    инициализация массива

---
+++ ex036_massiv/Program.cs
@@ -0,0 +1,22 @@
+﻿// работа с массивом
+
+//int[] numbers = {5, 6, 7, 8 ,9 ,10};//инициализация массива
+//PrintArray(numbers);
+int[] numbersA = {1, 2, 3};
+int[] numbersB = {10, 11, 12}; 
+
+numbersB = numbersA;
+numbersA[2] = 50;
+PrintArray(numbersA);
+PrintArray(numbersB);
+
+void PrintArray(int[] array)
+{
+    for (int i = 0; i < array.Length; i++)
+    {
+        Console.Write(array[i] + " ");
+    }
+}
+
+
+
