3964a0c8f2b46ee987eddb0c0d325c964be27306
Alex <A1ekcbend@gmail.com>
2022-08-19 10:48:36 +0300

    возведение произвольного числа в степень

---
+++ ex038_dz11/Program.cs
@@ -0,0 +1,25 @@
+﻿// Цикл который принимает на вход два числа  (А.В) и возводит А в натуральную степень В
+int result = 1;
+int i = 0;
+
+int Readint(string messege)//метод
+{
+    Console.WriteLine(messege);
+    return Convert.ToInt32(Console.ReadLine());
+}
+
+int Number = Readint("Введите число: ");
+int Stepen = Readint("Введите степень: ");
+
+
+    while (i < Stepen)
+    {
+        result = result * Number;
+        i++;
+    }
+    //return result;
+Console.WriteLine(result);
+
+
+
+
