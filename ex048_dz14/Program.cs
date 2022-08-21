3dde6d83cf540e90e5dafeedec67275a1ebaf654
Alex <a1eksbend@gmail.com>
2022-08-22 00:14:21 +0300

    вывод количества чётных чисел

---
+++ ex048_dz14/Program.cs
@@ -0,0 +1,42 @@
+﻿// Задать массив заполненный случайными положительными трёхзначными числами. Написать программу которая покажет количество чётных чисел в массиве.
+
+int size = ReadInt("Ввдите размерность массива: ");
+int [] numbers = new int[size];
+
+FillArrayRandomNumbers(numbers);
+PrintArray(numbers);
+int result = 0;
+
+for (int i = 0; i < numbers.Length; i++)
+    {
+        if (numbers[i] % 2 == 0 )
+        {
+            result++;
+        }   
+    }Console.WriteLine($"В массиве {result} чётных символов");
+
+
+
+
+void FillArrayRandomNumbers(int [] array)// рандом числа в массив
+{
+    for (int i = 0; i < array.Length; i++)
+    {
+        array[i] = new Random().Next(99, 999);
+    }
+}
+
+void PrintArray(int[] array)//вывод массива на экран
+{
+    for (int i = 0; i < array.Length; i++)
+    {
+        Console.Write(array[i] + " ");
+    }
+Console.WriteLine();
+}
+
+int ReadInt(string message)
+{
+    Console.Write(message);
+    return Convert.ToInt32(Console.ReadLine());
+}
