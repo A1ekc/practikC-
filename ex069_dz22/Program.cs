e84c4a40183ceaf9e04f6c753c0c1d351cdd84b2
Alex <a1eksbend@gmail.com>
2022-09-02 07:06:22 +0300

    сартировка по убыванию

---
+++ ex069_dz22/Program.cs
@@ -0,0 +1,59 @@
+﻿// Задать двумерный массив. Программа упорядочит по убыванию элементы каждой строки 
+
+int rows = ReadInt("Введите количество строк: ");
+int colums = ReadInt("Введите количество столбцов: ");
+int[,] numbers = new int[rows, colums];
+FillArray2D(numbers);
+PrintArray2D(numbers);
+SortArray(numbers);
+PrintArray2D(numbers);
+
+//сотрируем построчно
+void SortArray(int[,] array)//сортирууем массив от максимума к минимуму//пузырьковая сортировка
+{
+    for (int i = 0; i < array.GetLength(0); i++)//GetLength(0) - строки
+    {
+        for (int j = 0; j < array.GetLength(1); j++)//GetLength(1) - столбцы
+        {
+            for ( int k = 0; k < array.GetLength(0); k++)
+            {    
+                    if (array[i, j] <= array[i, k]) continue;
+                    {
+                        int temp = array[i, j];
+                        array[i, j] = array[i, k];
+                        array[i, k] = temp;
+                    }
+            }
+        }
+    }
+}
+
+void FillArray2D(int[,] array)
+{
+    for (int i = 0; i < array.GetLength(0); i++)//GetLength(0) - строки
+    {
+        for (int j = 0; j < array.GetLength(1); j++)//GetLength(1) - столбцы
+        {
+            array[i, j] = new Random().Next(0, 5);
+        }
+    }
+}
+
+void PrintArray2D(int[,] array)
+{
+    for (int i = 0; i < array.GetLength(0); i++)//GetLength(0) - строки
+    {
+        for (int j = 0; j < array.GetLength(1); j++)//GetLength(1) - столбцы
+        {
+            Console.Write(array[i, j] + " ");
+        }
+        Console.WriteLine();
+    }
+    Console.WriteLine();
+}
+
+int ReadInt(string message)
+{
+    Console.Write(message);
+    return Convert.ToInt32(Console.ReadLine());
+}
