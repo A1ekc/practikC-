3801cb3fc97c19d2f79e9e32dd43bee2af8827fc
Alex <a1eksbend@gmail.com>
2022-09-03 01:06:38 +0300

    рекурсия цифры от 1 до N

---
+++ ex074_recurci9_4icla1toN/Program.cs
@@ -0,0 +1,35 @@
+﻿// Задать значение N Напишите программу, которая выведет все натуральные числа в промежутке от 1 до N
+
+/*//Первый вариант
+Console.Write("Введите число: ");
+int number = Convert.ToInt32(Console.ReadLine());
+Console.Write(NaturalNumber(number));
+
+int NaturalNumber(int n)
+{
+    if(n == 1)
+    return 1;
+    else
+    {
+        Console.Write(NaturalNumber(n-1) +  " ");
+    }
+
+    return n;
+}
+*/
+//второй вариант
+Console.Write("Введите число: ");
+int number = Convert.ToInt32(Console.ReadLine());
+NaturalNumber(number);
+
+void NaturalNumber(int n)
+{
+    if(n == 1)
+        Console.Write(n + " ");
+    else
+    {   
+        NaturalNumber(n - 1);
+        Console.Write(n + " ");
+        
+    }
+}
