6cd955139df3cf2b9552beb52b07d9065c225200
Alex <a1eksbend@gmail.com>
2022-09-04 18:04:09 +0300

    все числа от N до 1

---
+++ ex079_dz27/Program.cs
@@ -0,0 +1,27 @@
+﻿// Задать значение N. Написать программу которая выведет все натуральные числа в промежутке от N до 1.
+
+int number = ReadInt("Введите минимальное значение: ");
+
+int ReadInt(string message)
+{
+    Console.Write(message);
+    return   Convert.ToInt32(Console.ReadLine());
+}
+
+NaturalNumber(number);
+
+void NaturalNumber(int number)
+{
+
+    if (number > 1)
+    {
+        Console.Write($"{number}, ");
+        NaturalNumber(number - 1);
+    }
+
+    if (number == 1)
+    {
+        Console.Write($"{number} ");
+    }
+
+}
