1706c1adea73ce4745254e21b104d32089ae9114
Alex <a1ekcbend@gmail.com>
2022-09-05 14:38:44 +0300

    сумма элементов от M до N

--- ex80_dz28/Program.cs
+++ ex80_dz28/Program.cs
@@ -9,6 +9,31 @@ int ReadInt(string message)
     return   Convert.ToInt32(Console.ReadLine());
 }
 
+
+///Метод нахождения суммы натуральных элементов в промежутке от M до N
+void ResultMN (int m, int n, int sum)
+{
+    if (m > n) 
+    {
+        Console.WriteLine($"Сумма натуральных элементов в промежутке от M до N: {sum}"); 
+        return;
+    }
+    sum = sum + (m++);
+    ResultMN(m, n, sum);
+}
+
+ResultMN(m, n, 0);
+
+/*
+int m = ReadInt("Введите минимальное значение: ");
+int n = ReadInt("Введите максимальное значение: ");
+
+int ReadInt(string message)
+{
+    Console.Write(message);
+    return   Convert.ToInt32(Console.ReadLine());
+}
+
 NaturalNumber(m, n);
 
 int NaturalNumber(int m, int n)
@@ -17,29 +42,25 @@ int NaturalNumber(int m, int n)
     if (m < n)
     {   
          sum += m;
+         return sum;
         NaturalNumber(m + 1, n);//потом вызываем опять и передаём m+1
     }
 
     if (m > n)
     {
         sum += m;
+        return sum;
         NaturalNumber(m - 1, n);
     }
 
-    if (m == n)
-    {
-        sum = 0;
-        Console.Write($"{0} ");
-    }
+    //if (m == n)
+    //{
+    //    sum = 0;
+    //    Console.Write($"{0} ");
+    //}
 return sum;
-
 }
-//Console.Write($"{sum}, ");
+Console.Write($"{sum}, ");
 //Console.WriteLine("Сумма чисел равна: " + NaturalNumber(sum));
+*/
 
-int sumTo(int n)
-{
-    if(n == 1)
-        return 1;
-    return n + sumTo(n-1);
-}
